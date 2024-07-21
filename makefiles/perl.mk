c511c422ce9903fe9289167b9659dbe4f08f1fa2
johnnyp382 <93363960+johnnyp382@users.noreply.github.com>
2024-07-21 16:59:13 -0500

    Update perl.mk

--- makefiles/perl.mk
+++ makefiles/perl.mk
@@ -3,10 +3,10 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS  += perl
-PERL_MAJOR   := 5.32
+PERL_MAJOR   := 5.41
 PERL_VERSION := $(PERL_MAJOR).1
 PERL_API_V   := $(PERL_MAJOR).0
-PERL_CROSS_V := 1.3.5
+PERL_CROSS_V := 1.5.3
 DEB_PERL_V   ?= $(PERL_VERSION)
 
 export PERL_MAJOR
