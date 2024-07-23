445e3b7fe2875e427af305a3cc93c4e6d82d3a60
johnnyp382 <93363960+johnnyp382@users.noreply.github.com>
2024-07-23 16:47:23 -0500

    Update autoconf.mk

--- makefiles/autoconf.mk
+++ makefiles/autoconf.mk
@@ -7,12 +7,11 @@ AUTOCONF_VERSION  := 2.72
 DEB_AUTOCONF_V    ?= $(AUTOCONF_VERSION)
 
 autoconf-setup: setup
-	$(call DOWNLOAD_FILES,$(BUILD_SOURCE),https://ftpmirror.gnu.org/autoconf/autoconf-$(AUTOCONF_VERSION).tar.gz{$(comma).sig})
+	$(call DOWNLOAD_FILES,$(BUILD_SOURCE),https://git.savannah.gnu.org/cgit/autoconf.git/snapshot/autoconf-$(AUTOCONF_VERSION).tar.gz{$(comma).sig})
 	$(call PGP_VERIFY,autoconf-$(AUTOCONF_VERSION).tar.gz)
 	$(call EXTRACT_TAR,autoconf-$(AUTOCONF_VERSION).tar.gz,autoconf-$(AUTOCONF_VERSION),autoconf)
 	sed -i 's/libtoolize/glibtoolize/g' $(BUILD_WORK)/autoconf/bin/autoreconf.in
 	sed -i 's/libtoolize/glibtoolize/g' $(BUILD_WORK)/autoconf/man/autoreconf.1
-	$(call DO_PATCH,ucl,ucl,-p1)
 
 ifneq ($(wildcard $(BUILD_WORK)/autoconf/.build_complete),)
 autoconf:
