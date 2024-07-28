0115f855f24acc17c61c71b86799a4cff61df434
johnnyp382 <93363960+johnnyp382@users.noreply.github.com>
2024-07-28 17:44:52 -0500

    Update autoconf-archive.mk

--- makefiles/autoconf-archive.mk
+++ makefiles/autoconf-archive.mk
@@ -7,7 +7,7 @@ AUTOCONF-ARCHIVE_VERSION := 2023.02.20
 DEB_AUTOCONF-ARCHIVE_V   ?= $(AUTOCONF-ARCHIVE_VERSION)
 
 autoconf-archive-setup: setup
-	$(call DOWNLOAD_FILES,$(BUILD_SOURCE),https://git.savannah.gnu.org/cgit/autoconf-archive.git/snapshot/autoconf-archive-$(AUTOCONF-ARCHIVE_VERSION).tar.xz{$(comma).sig})
+	$(call DOWNLOAD_FILES,$(BUILD_SOURCE),https://mirrors.ibiblio.org/gnu/autoconf-archive/autoconf-archive-$(AUTOCONF-ARCHIVE_VERSION).tar.xz{$(comma).sig})
 	$(call PGP_VERIFY,autoconf-archive-$(AUTOCONF-ARCHIVE_VERSION).tar.xz)
 	$(call EXTRACT_TAR,autoconf-archive-$(AUTOCONF-ARCHIVE_VERSION).tar.xz,autoconf-archive-$(AUTOCONF-ARCHIVE_VERSION),autoconf-archive)
 
