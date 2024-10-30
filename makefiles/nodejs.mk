5dfc03bc6291ee8da144ea96c80bc2fa7965bb3e
johnnyp382 <93363960+johnnyp382@users.noreply.github.com>
2024-10-29 21:22:31 -0500

    Update nodejs.mk

--- makefiles/nodejs.mk
+++ makefiles/nodejs.mk
@@ -3,31 +3,35 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS        += nodejs
-NODEJS_VERSION     := 23.0.0
+NODEJS_VERSION     := 17.0.1
 DEB_NODEJS_V       ?= $(NODEJS_VERSION)
 
 SUBPROJECTS        += nodejs-lts
-NODEJS_LTS_VERSION := 20.18.0
+NODEJS_LTS_VERSION := 16.13.0
 DEB_NODEJS_LTS_V   ?= $(NODEJS_LTS_VERSION)
 
-NODEJS_HOST := mac
+ifeq ($(UNAME),Linux)
+NODEJS_HOST := linux
+endif
 
+ifeq ($(PLATFORM),iphoneos)
 NODEJS_TARGET := ios
+endif
 
 NODEJS_COMMON_FLAGS := \
-    --prefix=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
-    --dest-os=$(NODEJS_TARGET) \
-    --dest-cpu=$(MEMO_ARCH) \
-    --cross-compiling \
-    --without-npm \
-    --shared \
-    --shared-zlib \
-    --shared-libuv \
-    --shared-brotli \
-    --shared-nghttp2 \
-    --shared-cares \
-    --openssl-use-def-ca-store \
-    --with-intl=full-icu --download=all
+	--prefix=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
+	--dest-os=$(NODEJS_TARGET) \
+	--dest-cpu=$(MEMO_ARCH) \
+	--cross-compiling \
+	--without-npm \
+	--shared \
+	--shared-zlib \
+	--shared-libuv \
+	--shared-brotli \
+	--shared-nghttp2 \
+	--shared-cares \
+	--openssl-use-def-ca-store \
+	--with-intl=full-icu --download=all
 
 nodejs-setup: setup
 	$(call GITHUB_ARCHIVE,1Conan,node,v$(NODEJS_VERSION),v$(NODEJS_VERSION)-ios)
@@ -39,89 +43,79 @@ nodejs-lts-setup: setup
 
 ifeq (,$(NODEJS_HOST))
 nodejs:
-    @echo "nodejs building not supported on this host os."
+	@echo "nodejs building not supported on this host os."
 else ifeq (,$(NODEJS_TARGET))
 nodejs:
-    @echo "nodejs building not supported on this target os."
+	@echo "nodejs building not supported on this target os."
 else ifneq ($(wildcard $(BUILD_WORK)/nodejs/.build_complete),)
 nodejs:
-    @echo "Using previously built nodejs."
-ifdef USE_SYSTEM_LIBS
-nodejs: nodejs-setup
-    @echo "Using system libraries for nodejs build."
+	@echo "Using previously built nodejs."
 else
 nodejs: nodejs-setup nghttp2 openssl brotli libc-ares libuv1
-endif
-    cd $(BUILD_WORK)/nodejs;\
-    CC_host="$(CC_FOR_BUILD)" \
-    CXX_host="$(CXX_FOR_BUILD) -std=gnu++14" \
-    AR_host="$(AR_FOR_BUILD)" \
-    CFLAGS_host="$(CFLAGS_FOR_BUILD) -Wreturn-type" \
-    CXXFLAGS_host="$(CXXFLAGS_FOR_BUILD)" \
-    CPPFLAGS_host="$(CPPFLAGS_FOR_BUILD)" \
-    LDFLAGS_host="$(LDFLAGS_FOR_BUILD)" \
-    SDKROOT="$(TARGET_SYSROOT)" \
-    CXX="$(CXX) -std=gnu++14" \
-    CFLAGS="$(CFLAGS) -Wreturn-type -DOPENSSLDIR=$(MEMO_PREFIX)/etc/ssl" \
-    CXXFLAGS="$(CXXFLAGS)" \
-    LDFLAGS="$(LDFLAGS) -undefined dynamic_lookup" \
-    PKG_CONFIG_PATH="$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig" \
-    GYP_DEFINES="target_arch=$(MEMO_ARCH) host_os=$(NODEJS_HOST) target_os=$(NODEJS_TARGET)" \
-    ./configure \
-    $(if $(USE_SYSTEM_LIBS),--shared-nghttp2 --shared-openssl --shared-brotli --shared-libuv --shared-cares,) \
-        $(NODEJS_COMMON_FLAGS)
+	cd $(BUILD_WORK)/nodejs;\
+	CC_host="$(CC_FOR_BUILD)" \
+	CXX_host="$(CXX_FOR_BUILD) -std=gnu++14" \
+	AR_host="$(AR_FOR_BUILD)" \
+	CFLAGS_host="$(CFLAGS_FOR_BUILD) -Wreturn-type" \
+	CXXFLAGS_host="$(CXXFLAGS_FOR_BUILD)" \
+	CPPFLAGS_host="$(CPPFLAGS_FOR_BUILD)" \
+	LDFLAGS_host="$(LDFLAGS_FOR_BUILD)" \
+	SDKROOT="$(TARGET_SYSROOT)" \
+	CXX="$(CXX) -std=gnu++14" \
+	CFLAGS="$(CFLAGS) -Wreturn-type -DOPENSSLDIR=$(MEMO_PREFIX)/etc/ssl" \
+	CXXFLAGS="$(CXXFLAGS)" \
+	LDFLAGS="$(LDFLAGS) -undefined dynamic_lookup" \
+	PKG_CONFIG_PATH="$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig" \
+	GYP_DEFINES="target_arch=$(MEMO_ARCH) host_os=$(NODEJS_HOST) target_os=$(NODEJS_TARGET)" \
+	./configure \
+		$(NODEJS_COMMON_FLAGS)
 
-    +$(MAKE) USE_SYSTEM_LIBS=1 -C $(BUILD_WORK)/nodejs
-    +$(MAKE) USE_SYSTEM_LIBS=1 -C $(BUILD_WORK)/nodejs install \
-        DESTDIR=$(BUILD_STAGE)/nodejs
+	+$(MAKE) -C $(BUILD_WORK)/nodejs
+	+$(MAKE) -C $(BUILD_WORK)/nodejs install \
+		DESTDIR=$(BUILD_STAGE)/nodejs
 
-    mkdir -p $(BUILD_STAGE)/nodejs/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
-    cp -a $(BUILD_WORK)/nodejs/out/Release/node $(BUILD_STAGE)/nodejs/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
-    $(call AFTER_BUILD,copy)
+	mkdir -p $(BUILD_STAGE)/nodejs/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	cp -a $(BUILD_WORK)/nodejs/out/Release/node $(BUILD_STAGE)/nodejs/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	$(call AFTER_BUILD,copy)
 endif
 
 ifeq (,$(NODEJS_HOST))
 nodejs-lts:
-    @echo "nodejs-lts building not supported on this host os."
+	@echo "nodejs-lts building not supported on this host os."
 else ifeq (,$(NODEJS_TARGET))
 nodejs-lts:
-    @echo "nodejs-lts building not supported on this target os."
+	@echo "nodejs-lts building not supported on this target os."
 else ifneq ($(wildcard $(BUILD_WORK)/nodejs-lts/.build_complete),)
 nodejs-lts:
-    @echo "Using previously built nodejs-lts."
-ifdef USE_SYSTEM_LIBS
-nodejs: nodejs-setup
-    @echo "Using system libraries for nodejs build."
+	@echo "Using previously built nodejs-lts."
 else
-nodejs: nodejs-setup nghttp2 openssl brotli libc-ares libuv1
-endif
-    cd $(BUILD_WORK)/nodejs-lts;\
-    CC_host="$(CC_FOR_BUILD)" \
-    CXX_host="$(CXX_FOR_BUILD) -std=gnu++14" \
-    AR_host="$(AR_FOR_BUILD)" \
-    CFLAGS_host="$(CFLAGS_FOR_BUILD) -Wreturn-type" \
-    CXXFLAGS_host="$(CXXFLAGS_FOR_BUILD)" \
-    CPPFLAGS_host="$(CPPFLAGS_FOR_BUILD)" \
-    LDFLAGS_host="$(LDFLAGS_FOR_BUILD)" \
-    SDKROOT="$(TARGET_SYSROOT)" \
-    CXX="$(CXX) -std=gnu++14" \
-    CFLAGS="$(CFLAGS) -Wreturn-type" \
-    CXXFLAGS="$(CXXFLAGS)" \
-    LDFLAGS="$(LDFLAGS) -undefined dynamic_lookup" \
-    PKG_CONFIG_PATH="$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig" \
-    GYP_DEFINES="target_arch=$(MEMO_ARCH) host_os=$(NODEJS_HOST) target_os=$(NODEJS_TARGET)" \
-    ./configure \
-    $(if $(USE_SYSTEM_LIBS),--shared-nghttp2 --shared-openssl --shared-brotli --shared-libuv --shared-cares,) \
-        $(NODEJS_COMMON_FLAGS) \
-        --shared-openssl
+nodejs-lts: nodejs-lts-setup nghttp2 openssl brotli libc-ares libuv1
+	cd $(BUILD_WORK)/nodejs-lts;\
+	CC_host="$(CC_FOR_BUILD)" \
+	CXX_host="$(CXX_FOR_BUILD) -std=gnu++14" \
+	AR_host="$(AR_FOR_BUILD)" \
+	CFLAGS_host="$(CFLAGS_FOR_BUILD) -Wreturn-type" \
+	CXXFLAGS_host="$(CXXFLAGS_FOR_BUILD)" \
+	CPPFLAGS_host="$(CPPFLAGS_FOR_BUILD)" \
+	LDFLAGS_host="$(LDFLAGS_FOR_BUILD)" \
+	SDKROOT="$(TARGET_SYSROOT)" \
+	CXX="$(CXX) -std=gnu++14" \
+	CFLAGS="$(CFLAGS) -Wreturn-type" \
+	CXXFLAGS="$(CXXFLAGS)" \
+	LDFLAGS="$(LDFLAGS) -undefined dynamic_lookup" \
+	PKG_CONFIG_PATH="$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig" \
+	GYP_DEFINES="target_arch=$(MEMO_ARCH) host_os=$(NODEJS_HOST) target_os=$(NODEJS_TARGET)" \
+	./configure \
+		$(NODEJS_COMMON_FLAGS) \
+		--shared-openssl
 
-    +$(MAKE) USE_SYSTEM_LIBS=1 -C $(BUILD_WORK)/nodejs-lts
-    +$(MAKE) USE_SYSTEM_LIBS=1 -C $(BUILD_WORK)/nodejs-lts install \
-        DESTDIR=$(BUILD_STAGE)/nodejs
+	+$(MAKE) -C $(BUILD_WORK)/nodejs-lts
+	+$(MAKE) -C $(BUILD_WORK)/nodejs-lts install \
+		DESTDIR=$(BUILD_STAGE)/nodejs
 
-    mkdir -p $(BUILD_STAGE)/nodejs-lts/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
-    cp -a $(BUILD_WORK)/nodejs-lts/out/Release/node $(BUILD_STAGE)/nodejs-lts/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
-    $(call AFTER_BUILD,copy)
+	mkdir -p $(BUILD_STAGE)/nodejs-lts/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	cp -a $(BUILD_WORK)/nodejs-lts/out/Release/node $(BUILD_STAGE)/nodejs-lts/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	$(call AFTER_BUILD,copy)
 endif
 
 nodejs-package: nodejs-stage
