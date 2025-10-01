4a0e871e5c0ae476990ee690d7fb136f6b02810c
johnnyp382 <93363960+johnnyp382@users.noreply.github.com>
2025-10-01 17:49:24 -0500

    Update golang.mk

--- makefiles/golang.mk
+++ makefiles/golang.mk
@@ -3,9 +3,9 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS           += golang
-GOLANG_MAJOR_V        := 1.23
-GOLANG_VERSION        := $(GOLANG_MAJOR_V).0
-DEBIAN_GOLANG_VERSION := 1.24~2
+GOLANG_MAJOR_V        := 1.25
+GOLANG_VERSION        := $(GOLANG_MAJOR_V).1
+DEBIAN_GOLANG_VERSION := 1.25.1
 DEB_GOLANG_V          ?= $(GOLANG_VERSION)
 
 golang-setup: setup
